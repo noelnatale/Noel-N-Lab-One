3889fc0cc85fcff045bf43590b4e828eeda92583
noelnatale <natalen29@setoncatholic.org>
2026-09-18 16:04:39 +0000

    added an AI age comment

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 name = input("What is your name")
 print (name)
-age = input("What is your age")
+age = int(input("What is your age"))
 print (age)
 color = input("What is your favorite color")
 print (color)
-print("Hello" + name + ", you are" + age + "years old and your favorite color is")
+if age < 13:
+	age_comment = "You are a child."
+elif age < 20:
+	age_comment = "You are a teenager."
+elif age < 65:
+	age_comment = "You are an adult."
+else:
+	age_comment = "You are a senior."
+
+print("Hello " + name + ", you are " + str(age) + " years old. " + age_comment)
+print("Your favorite color is " + color + ".")
